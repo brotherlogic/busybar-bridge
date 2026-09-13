1c1f15fe8f934a797953a71cc98f347f651c3394
Brotherlogic Automation <brotherlogicautomation@gmail.com>
2026-09-12 22:17:51 -0700

    Add google.golang.org/protobuf dependency to go.mod
    Closes #34

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/brotherlogic/busybar-bridge
 
 go 1.27.1
 
-require github.com/coder/websocket v1.8.14
+require (
+	github.com/coder/websocket v1.8.14
+	google.golang.org/protobuf v1.36.5
+)
--- main_test.go
+++ main_test.go
@@ -4,10 +4,18 @@ import (
 	"testing"
 
 	"github.com/coder/websocket"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestWebsocketDependency(t *testing.T) {
 	if websocket.StatusCode(0) != 0 {
 		t.Fatal("unexpected status code")
+	}
+}
+
+func TestProtobufDependency(t *testing.T) {
+	var m proto.Message
+	if proto.Size(m) != 0 {
+		t.Fatal("unexpected proto size")
 	}
 }
